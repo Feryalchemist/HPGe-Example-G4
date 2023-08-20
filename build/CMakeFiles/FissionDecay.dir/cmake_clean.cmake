5df3b3d326619d05e15297ea497f5542702bbef8
Feryalchemist <83800595+Feryalchemist@users.noreply.github.com>
2023-08-20 18:03:45 +0700

    Add files via upload

---
+++ build/CMakeFiles/FissionDecay.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FissionDecay"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/FissionDecay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Simulation.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Simulation.dir/Simulation.cc.o"
+  "CMakeFiles/Simulation.dir/Simulation.cc.o.d"
+  "CMakeFiles/Simulation.dir/src/Action.cc.o"
+  "CMakeFiles/Simulation.dir/src/Action.cc.o.d"
+  "CMakeFiles/Simulation.dir/src/Detector.cc.o"
+  "CMakeFiles/Simulation.dir/src/Detector.cc.o.d"
+  "CMakeFiles/Simulation.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/Simulation.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/Simulation.dir/src/Event.cc.o"
+  "CMakeFiles/Simulation.dir/src/Event.cc.o.d"
+  "CMakeFiles/Simulation.dir/src/Hit.cc.o"
+  "CMakeFiles/Simulation.dir/src/Hit.cc.o.d"
+  "CMakeFiles/Simulation.dir/src/ParticleGenerator.cc.o"
+  "CMakeFiles/Simulation.dir/src/ParticleGenerator.cc.o.d"
+  "CMakeFiles/Simulation.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/Simulation.dir/src/PhysicsList.cc.o.d"
+  "CMakeFiles/Simulation.dir/src/Run.cc.o"
+  "CMakeFiles/Simulation.dir/src/Run.cc.o.d"
+  "Simulation"
+  "Simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
